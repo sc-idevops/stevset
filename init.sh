746cf05696f287b8df9f4a619b9c64c06aea476f
steveokard <steveokard@gmail.com>
2017-09-24 15:06:08 -0400

    init git submodules, because I never remember to

--- init.sh
+++ init.sh
@@ -35,7 +35,10 @@ then
 fi
 
 #init vim and plugins
+git submodule init
+git submodule update
 vim +PluginInstall! +qall
+mv ~/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-clvv-SLASH-fasd/fasd ~/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-clvv-SLASH-fasd/fasd.zsh
 
 #init git
 echo -n "Would you like to configure your git name and email? (y/n) => "; read answer
