65455490a5e7316f4c96e55c419727608906d1f8
steveokard <steveokard@gmail.com>
2017-09-27 14:18:30 -0400

    package patch integrated into spacemacs script

--- packagepatch.zsh
+++
@@ -1,4 +0,0 @@
-cp package.patch /home/stev/.emacs.d/layers/+themes/themes-megapack/
-cd /home/`whoami`/.emacs.d/layers/+themes/themes-megapack
-patch < package.patch
-rm package.patch
--- spacemacs.zsh
+++ spacemacs.zsh
@@ -1,3 +1,9 @@
 ln -s /home/`whoami`/stevset/.spacemacs /home/`whoami`/.spacemacs
 #init spacemacs in the background
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+
+#Patch theme file to squelch error messages
+cp package.patch /home/stev/.emacs.d/layers/+themes/themes-megapack/
+cd /home/`whoami`/.emacs.d/layers/+themes/themes-megapack
+patch < package.patch
+rm package.patch
