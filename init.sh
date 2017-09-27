d79ff7722a50c240d7516facb879e01629f6e628
steveokard <steveokard@gmail.com>
2017-09-27 14:40:02 -0400

    antibody line didn't belong with vim's script

--- init.sh
+++ init.sh
@@ -40,6 +40,8 @@ fi
 echo "downloading antibody"
 curl -sL https://git.io/antibody | bash -s
 #echo 'source <(antibody init)' >> ~/.zshrc
+#Enable fasd to be loaded by changing it's file extension
+mv ~/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-clvv-SLASH-fasd/fasd ~/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-clvv-SLASH-fasd/fasd.zsh
 
 #Install fonts
 if [[ -n "$SSH_CLIENT" ]]
--- vim.zsh
+++ vim.zsh
@@ -2,4 +2,4 @@
 git submodule init
 git submodule update
 vim +PluginInstall! +qall
-mv ~/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-clvv-SLASH-fasd/fasd ~/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-clvv-SLASH-fasd/fasd.zsh
+
