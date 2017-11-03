05f4ff1d32dbb16cf22c8b29e846732c9a7d860b
steveokard <steveokard@gmail.com>
2017-11-03 00:49:49 -0400

    fixed installing ssh, added apt switch -m

--- deps.zsh
+++ deps.zsh
@@ -2,7 +2,7 @@
 #These are the base dependencies for any install
 
 echo "installing base dependencies"
-sudo apt-get install openssh \
+sudo apt-get -m install openssh-server \
 	tmux \
 	zsh \
         vim \
