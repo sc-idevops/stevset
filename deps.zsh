26cc1211d23c279858c972d1dc79963835e47d0b
steveokard <steveokard@gmail.com>
2018-03-01 15:28:48 -0500

    simplify starting tmux with byobu

--- deps.zsh
+++ deps.zsh
@@ -9,13 +9,10 @@ sudo apt-get -m install openssh-server \
 	powerline\
 	htop \
 	mc \
-	curl
-#check if on a ssh connection, if you are, emacs doesnt need X
-echo "installing emacs, based on X support"
+	curl \
+	byobu \
+	emacs
 
-if [[ -n "$SSH_CLIENT" ]]
-then
-	sudo apt-get install emacs24-nox
-else
-	sudo apt-get install emacs
-fi
+echo "enabling byobu"
+byobu-enable
+
