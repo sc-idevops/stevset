a7c8aaa93d3f5f6513b02505e1c827f83f798f8d
steveokard <steveokard@gmail.com>
2018-04-11 15:27:33 -0400

    quick push

--- prezto.zsh
+++ prezto.zsh
@@ -3,9 +3,15 @@
 
 # Clone Prezto
 git clone --recursive https://github.com/steveokard/prezto.git "${ZDOTDIR:-$HOME}/stevset/prezto/.zprezto"
+
+#Remove old stuff
+rm -rf ~/.zshrc ~/.zsh
+
 #Make Environment Links, per github readme
 setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/stevset/prezto/.zprezto/runcoms/^README.md(.N); do
 	  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
   done
 
+#Fix History
+cat ~/.zsh_history > ~/.zhistory
