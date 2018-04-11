a96c8978ceb8ab854778586c4f87819f9b1ac9c9
steveokard <steveokard@gmail.com>
2018-04-11 14:27:33 -0400

    pretzo transitioning

--- prezto.zsh
+++ prezto.zsh
@@ -2,7 +2,8 @@
 #Initializes Prezto
 #make link to stevset
 stow prezto
-
+# Clone Prezto
+git clone --recursive https://github.com/steveokard/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 #Make Environment Links, per github readme
 setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
