513e70f57a9734f673d4e0bc0e421638de58ca26
steveokard <steveokard@gmail.com>
2018-04-11 14:45:52 -0400

    configuration for prezto

--- init.sh
+++ init.sh
@@ -3,13 +3,16 @@
 echo "installing required packages"
 ./deps.zsh
 
-#remove existing configs, if they exist
+#setup prezto
+zsh prezto.zsh
+
+#create symlinks using stow
 echo "Stowing Configs"
 stow tmux
 stow vim
-stow zsh
 stow emacs
 stow bash
+stow prezto
 
 #set new shell
 echo "set new shell to zsh"
@@ -18,22 +21,6 @@ then
 	chsh -s /bin/zsh
 fi
 
-#init git
-echo -n "Would you like to configure your git name and email? (y/n) => "; read answer
-if [[ $answer = "Y" ]] || [[ $answer = "y" ]]; then
-    echo -n "What is your git user name => "; read name
-    git config --global user.name "$name"
-    echo -n "What is your git email => "; read email
-    git config --global user.email "$email"
-fi
-
-#Download Antibody
-echo "downloading antibody"
-curl -sL https://git.io/antibody | bash -s
-#echo 'source <(antibody init)' >> ~/.zshrc
-#Enable fasd to be loaded by changing it's file extension
-mv ~/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-clvv-SLASH-fasd/fasd ~/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-clvv-SLASH-fasd/fasd.zsh
-
 #Install fonts
 if [[ -n "$SSH_CLIENT" ]]
 then
@@ -41,9 +28,6 @@ then
 else
 	./fonts.zsh
 fi
-
-#init vim and plugins
-./vim.zsh
 
 #init spacemacs in the background
 ./spacemacs.zsh
--- prezto.zsh
+++ prezto.zsh
@@ -2,10 +2,10 @@
 #Initializes Prezto
 
 # Clone Prezto
-git clone --recursive https://github.com/steveokard/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+git clone --recursive https://github.com/steveokard/prezto.git "${ZDOTDIR:-$HOME}/stevset/prezto/.zprezto"
 #Make Environment Links, per github readme
 setopt EXTENDED_GLOB
-for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+for rcfile in "${ZDOTDIR:-$HOME}"/stevset/prezto/.zprezto/runcoms/^README.md(.N); do
 	  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
   done
 
