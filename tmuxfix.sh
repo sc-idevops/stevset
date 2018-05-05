c0ef3caf7758ea7cdf406b1786d7aa49c0b32ea7
steveokard <steveokard@gmail.com>
2018-05-05 01:05:42 -0400

    fixes powerline issue on xenial

---
+++ tmuxfix.sh
@@ -0,0 +1,6 @@
+wget http://security.ubuntu.com/ubuntu/pool/universe/p/powerline/fonts-powerline_2.6-1_all.deb
+wget http://security.ubuntu.com/ubuntu/pool/universe/p/powerline/powerline_2.6-1_amd64.deb
+wget http://security.ubuntu.com/ubuntu/pool/universe/p/powerline/python-powerline_2.6-1_all.deb
+wget http://security.ubuntu.com/ubuntu/pool/universe/p/powerline/python3-powerline_2.6-1_all.deb
+sudo dpkg -i fonts-powerline_2.6-1_all.deb powerline_2.6-1_amd64.deb python-powerline_2.6-1_all.deb python3-powerline_2.6-1_all.deb
+rm fonts-powerline_2.6-1_all.deb powerline_2.6-1_amd64.deb python-powerline_2.6-1_all.deb python3-powerline_2.6-1_all.deb
