67ba2db3e58d437fdb1d70dff2a80b573d884da5
steveokard <steveokard@gmail.com>
2018-05-05 01:16:35 -0400

    added killing powerline daemon

--- tmuxfix.sh
+++ tmuxfix.sh
@@ -4,3 +4,4 @@ wget http://security.ubuntu.com/ubuntu/pool/universe/p/powerline/python-powerlin
 wget http://security.ubuntu.com/ubuntu/pool/universe/p/powerline/python3-powerline_2.6-1_all.deb
 sudo dpkg -i fonts-powerline_2.6-1_all.deb powerline_2.6-1_amd64.deb python-powerline_2.6-1_all.deb python3-powerline_2.6-1_all.deb
 rm fonts-powerline_2.6-1_all.deb powerline_2.6-1_amd64.deb python-powerline_2.6-1_all.deb python3-powerline_2.6-1_all.deb
+powerline-daemon -k
