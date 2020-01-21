59b7e1b5c4a9350f377a436d651d5fb886d0d1bb
steveokard <steveokard@gmail.com>
2020-01-21 15:37:55 -0500

    added test cases for installation

--- emacs_doom.zsh
+++ emacs_doom.zsh
@@ -1,6 +1,17 @@
 #!/bin/zsh
 #This will install Doom for Emacs
-git clone https://github.com/hlissner/doom-emacs ~/.emacs.d
-#Test for emacs and ripgrep
 
-~/.emacs.d/bin/doom install
+if [ -e ~/.emacs.d/spacemacs.mk ]
+#cleanup old spacemacs
+    mv ~/.emacs.d ~/spacemacs.d
+else
+    git clone https://github.com/hlissner/doom-emacs ~/.emacs.d
+fi 
+#Test for ripgrep
+if [ -e /usr/bin/rg ]
+    ~/.emacs.d/bin/doom install
+else 
+    echo "Ripgrep not detected, attempting to install!"
+    sudo apt install ripgrep
+fi
+
