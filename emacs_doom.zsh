d2dc3bc91cbd3f6a9c48d15bde40860e48a0d3aa
steveokard <steveokard@gmail.com>
2020-01-23 22:18:26 -0500

    will be easier to just ask

--- emacs_doom.zsh
+++ emacs_doom.zsh
@@ -1,19 +1,17 @@
 #!/bin/zsh
 #This will install Doom for Emacs
-#which version of ubuntu are we on?
-if [ `lsb_release -rs` -lt 19 ] #needs to be an int vs int
-    echo "Forget it! You'll have to install an updated version of emacs"
-    exit 1
-fi 
+#sudo add-apt-repository ppa:kelleyk/emacs
 #cleanup old spacemacs
 if [ -e ~/.emacs.d/spacemacs.mk ]
-    mv ~/.emacs.d ~/spacemacs.d
+    then
+      mv ~/.emacs.d ~/spacemacs.d
 fi
 
 git clone https://github.com/hlissner/doom-emacs ~/.emacs.d
 
 #Test for ripgrep
 if [ -e /usr/bin/rg ]
+then
     ~/.emacs.d/bin/doom install
 else 
     echo "Ripgrep not detected, attempting to install!"
