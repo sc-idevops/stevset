f3566e6dffb9e165ddfaabef3cda10a50607a13f
steveokard <steveokard@gmail.com>
2020-04-15 21:51:59 -0400

    Revert "minor updates"
    This reverts commit 3d1a7d8b53ea3a735b1ffed4d7393dc7f2a126d3.

--- update.zsh
+++ update.zsh
@@ -1,1 +1,9 @@
 #!/bin/zsh
+rm ~/.ssh/config
+stow config
+sudo systemctl daemon-reload
+systemctl --user enable emacs
+systemctl --user start emacs
+systemctl --user status emacs
+echo "you should start emacs and update all the packages!"
+
