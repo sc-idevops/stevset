f5feae211d4cd11bd42c4b2fe9445efa4cf9657c
steveokard <steveokard@gmail.com>
2020-08-07 05:00:52 -0400

    emoji scripting

--- update.zsh
+++ update.zsh
@@ -1,4 +1,10 @@
 #!/bin/zsh
-sudo apt install ripgrep
-(zsh emacs_doom.zsh)
-
+echo "Configuring Emoji Fonts"
+git clone https://gitlab.com/es20490446e/emoji.conf.git
+cd emoji.conf/
+./install-uninstall.sh
+#regenerate font cache
+fc-cache -f -v
+#cleanup
+cd ..
+rm -r emoji.conf
