6e83ae46de4c5cec2f46f834bf14cfb19cbb2662
steveokard <steveokard@gmail.com>
2020-08-21 22:25:34 -0400

    emoji fix script

---
+++ scripts/emojifix.zsh
@@ -0,0 +1,8 @@
+#This will finally fix this problem
+echo "cloning emoji.conf"
+git clone https://gitlab.com/es20490446e/emoji.conf.git
+cd emoji.conf
+./install-uninstall.sh
+cd ..
+rm -rf emoji.conf
+#test by going to https://www.fileformat.info/info/emoji/browsertest.htm
