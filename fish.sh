62f9bceb554d42ebe09cef42924fab1f1db2eec0
steveokard <steveokard@gmail.com>
2020-11-15 21:08:33 -0500

    set executable

--- fish.sh
+++ fish.sh
@@ -2,7 +2,6 @@
 #installs and sets up oh-my-fish
 sudo apt install git fish
 curl -L https://get.oh-my.fish | fish
-#omf theme install bobthefish 
 ln -s "$HOME/stevset/fish/omf" "$HOME/.config/omf"
-omf install
+omf theme install bobthefish 
 
