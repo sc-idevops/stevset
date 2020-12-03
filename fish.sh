a8cd608f8a5892702129cc357fa228f8281fc6af
steveokard <steveokard@gmail.com>
2020-12-02 21:31:26 -0500

    fixed bug and added aliases

--- fish.sh
+++ fish.sh
@@ -2,6 +2,6 @@
 #installs and sets up oh-my-fish
 sudo apt install git fish
 curl -L https://get.oh-my.fish | fish
-ln -s "$HOME/stevset/fish/omf" "$HOME/.config/omf"
+ln -s "$HOME/stevset/fish/omf" "$HOME/.config/"
 omf install bobthefish 
 
---
+++ fish/omf/init.fish
@@ -0,0 +1,32 @@
+#Readability aliases
+alias lss='ls -alh --color=auto'
+alias mount='mount |column -t'
+alias df='df -H'
+
+#Shell Function aliases
+alias dirs="dirs -v"
+alias h='history'
+alias j='jobs -l'
+
+#shortcuts aliases
+alias please="sudo !!"
+alias ping='ping -c 5'
+alias ax="chmod a+x"
+alias wmip="curl icanhazip.com"
+
+#git aliases
+alias gitS='git status'
+alias gitL='git log'
+alias gitD='git diff'
+alias gitC='git commit -a'
+
+#Apt aliases
+alias aptup="sudo apt update && sudo apt upgrade"
+alias aptrm="sudo apt autoremove && sudo apt autoclean"
+
+#ssh aliases
+alias sagent="eval `ssh-agent`"
+
+#Command Replacement aliases
+alias less='less -imJMW'
+
