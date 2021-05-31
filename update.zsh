c276248decdef2da69b99d55bdf5dc07ad9544b1
steveokard <steveokard@gmail.com>
2021-05-30 22:08:12 -0400

    quick fix to command order

--- update.zsh
+++ update.zsh
@@ -3,9 +3,9 @@ echo "Remove Doom"
 rm -r  --interactive=never $HOME/.emacs.d $HOME/.doom.d
 
 echo "Install zinit"
-rm ~/.zshrc ~/.zpreztorc ~/.zlogin ~/.zlogout ~/.zprofile ~/.zshenv
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
 #This will install Zinit in ~/.zinit/bin. .zshrc will be updated with three lines of code that wi       │ ll be added to the bottom. The lines will be sourcing zinit.zsh and setting up completion for com       │ mand zinit. After installing and reloading the shell compile Zinit with zinit self-update. 
+rm ~/.zshrc ~/.zpreztorc ~/.zlogin ~/.zlogout ~/.zprofile ~/.zshenv
 ln ~/stevset/zsh/.zshrc ~/.zshrc 
 ln ~/stevset/zsh/aliases.zsh ~/.zsh_aliases
 source ~/.zshrc
