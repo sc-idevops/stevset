0f23e62e86732223b3500cab2060682aa2689c59
steveokard <steveokard@gmail.com>
2021-06-02 22:02:28 -0400

    added zprofile back

--- update.zsh
+++ update.zsh
@@ -6,6 +6,7 @@ echo "Install zinit"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
 #This will install Zinit in ~/.zinit/bin. .zshrc will be updated with three lines of code that wi       │ ll be added to the bottom. The lines will be sourcing zinit.zsh and setting up completion for com       │ mand zinit. After installing and reloading the shell compile Zinit with zinit self-update. 
 rm ~/.zshrc ~/.zpreztorc ~/.zlogin ~/.zlogout ~/.zprofile ~/.zshenv
-ln ~/stevset/zsh/.zshrc ~/.zshrc 
-ln ~/stevset/zsh/aliases.zsh ~/.zsh_aliases
+ln -s ~/stevset/zsh/.zshrc ~/.zshrc 
+ln -s ~/stevset/zsh/aliases.zsh ~/.zsh_aliases
+ln -s ~/stevset/zsh/.zprofile ~/.zprofile
 source ~/.zshrc
