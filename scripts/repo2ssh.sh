ab002e27c596b6e7e83406098a0f84335ada9f12
steveokard <steveokard@gmail.com>
2021-06-04 23:42:09 -0400

    script can be used on any git repo

--- scripts/repo2ssh.sh
+++ scripts/repo2ssh.sh
@@ -1,2 +1,7 @@
 #this command changes the git repo from HTTPS to SSH so you can use the key instead of typing in your password all the time.
-git remote set-url origin git@github.com:steveokard/stevset.git
+USER=steveokard
+if [ -n "$1" ] 
+then
+  git remote set-url origin git@github.com:$USER/$1.git
+  git remote -v
+fi
