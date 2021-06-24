d5a446a07d523d5d1f9b02af927851bd679d6b64
steveokard <steveokard@gmail.com>
2021-06-24 03:06:16 -0400

    test statements

--- udeps.sh
+++ udeps.sh
@@ -43,10 +43,11 @@ read -n1 -p $'\nDoes this system have a GUI?\n' REPLY
 	  sudo apt-get install emacs-nox
 	fi
 #Fix stupid Rust issue
-if [ -f "/usr/.crates2.json" ]
+if [ -f "/usr/.crates2.json" ]; then
+  echo "crates"
   sudo sed -i '/crates2/d' /var/lib/dpkg/info/ripgrep.list
   sudo rm -f /usr/.crates2.json
-  sudo apt install bat
+  sudo apt-get -my install bat
   sudo sed -i '/crates2/d' /var/lib/dpkg/info/bat.list
   sudo rm -f /usr/.crates2.json
 fi
@@ -54,9 +55,10 @@ fi
 #This section installs software outside apt
 
 #select exa or lsd
+if [[ ! "which exa > /dev/null" ]]; then
 dpkg_url http://mirrors.kernel.org/ubuntu/pool/universe/r/rust-exa/exa_0.9.0-4_amd64.deb
 # wget https://github.com/Peltoche/lsd/releases/download/0.20.1/lsd_0.20.1_amd64.deb; sudo dpkg -i ./lsd_0.20.1_amd64.deb; rm lsd_0.20.1_amd64.deb
 dpkg_url https://github.com/muesli/duf/releases/download/v0.6.2/duf_0.6.2_linux_amd64.deb
 dpkg_url https://github.com/ClementTsang/bottom/releases/download/0.6.1/bottom_0.6.1_amd64.deb
 #https://github.com/dandavison/delta/releases/download/0.8.0/delta-0.8.0-x86_64-unknown-linux-gnu.tar.gz
-
+fi
