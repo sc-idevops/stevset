149376aca64f36e224f7c78a58346e680ecb2adb
steveokard <steveokard@gmail.com>
2021-08-21 21:15:53 -0400

    updated external packages

--- udeps.sh
+++ udeps.sh
@@ -13,24 +13,24 @@ function dpkg_url() {
 
 # installing base dependencies
 sudo apt-get -my install \
-    git \
+  git \
 	stow \
 	etckeeper \
 	tmux \
 	zsh \
-    vim \
+  vim \
 	powerline\
 	htop \
 	mc \
 	curl \
 	aptitude \
-    nnn \
-    ack \
-    fzf \
-    ripgrep \
-    grc \
-    fd-find \
-    ncdu 
+  nnn \
+  ack \
+  fzf \
+  ripgrep \
+  grc \
+  fd-find \
+  ncdu 
 
 read -n1 -p $'Does this system need a ssh server?\n' REPLY
 	if [[ $REPLY == [Yy] ]]; then 
@@ -52,7 +52,7 @@ if [ -f "/usr/.crates2.json" ]; then
 fi
 
 #This section installs software outside apt
-dpkg_url http://mirrors.kernel.org/ubuntu/pool/universe/r/rust-exa/exa_0.9.0-4_amd64.deb
+dpkg_url http://mirrors.kernel.org/ubuntu/pool/universe/r/rust-exa/exa_0.9.0-5_amd64.deb
 dpkg_url https://github.com/muesli/duf/releases/download/v0.6.2/duf_0.6.2_linux_amd64.deb
-dpkg_url https://github.com/ClementTsang/bottom/releases/download/0.6.1/bottom_0.6.1_amd64.deb
+dpkg_url https://github.com/ClementTsang/bottom/releases/download/0.6.3/bottom_0.6.3_amd64.deb
 #https://github.com/dandavison/delta/releases/download/0.8.0/delta-0.8.0-x86_64-unknown-linux-gnu.tar.gz
