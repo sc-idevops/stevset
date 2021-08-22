d791d4428796edd6a3aa0b9c5a1b7d1fd9a413af
steveokard <steveokard@gmail.com>
2021-08-21 21:24:58 -0400

    update external packages

--- udeps.sh
+++ udeps.sh
@@ -55,4 +55,4 @@ fi
 dpkg_url http://mirrors.kernel.org/ubuntu/pool/universe/r/rust-exa/exa_0.9.0-5_amd64.deb
 dpkg_url https://github.com/muesli/duf/releases/download/v0.6.2/duf_0.6.2_linux_amd64.deb
 dpkg_url https://github.com/ClementTsang/bottom/releases/download/0.6.3/bottom_0.6.3_amd64.deb
-#https://github.com/dandavison/delta/releases/download/0.8.0/delta-0.8.0-x86_64-unknown-linux-gnu.tar.gz
+dpkg_url https://github.com/dandavison/delta/releases/download/0.8.3/git-delta_0.8.3_amd64.deb
--- update.zsh
+++ update.zsh
@@ -1,8 +1,17 @@
 #!/bin/zsh
-#delete current zsh config
-ln -s ~/stevset/prezto/.zprezto ~/.zprezto
-./udeps.sh
-./prezto.zsh
+#Update external packages
 
-#Tmux Theme
-git clone https://github.com/wfxr/tmux-power.git "$HOME/stevset/tmux/t-theme"
+function dpkg_url() {
+  local tmp_deb="$(mktemp)"
+  local src_url=$1
+  local args=${@:2}
+
+  wget -O $tmp_deb $src_url &&
+  sudo dpkg -i $tmp_deb $args &&
+  { rm -f $tmp_deb; true; } || # commands above succeeded, remove tmp file
+  { rm -f $tmp_deb; false; }   # commands above failed, remove tmp file anyway
+}
+dpkg_url http://mirrors.kernel.org/ubuntu/pool/universe/r/rust-exa/exa_0.9.0-5_amd64.deb
+dpkg_url https://github.com/muesli/duf/releases/download/v0.6.2/duf_0.6.2_linux_amd64.deb
+dpkg_url https://github.com/ClementTsang/bottom/releases/download/0.6.3/bottom_0.6.3_amd64.deb
+dpkg_url https://github.com/dandavison/delta/releases/download/0.8.3/git-delta_0.8.3_amd64.deb
