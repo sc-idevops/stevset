36a0db66a005aeae5bb50b9971bff31f5911bf4f
steveokard <steveokard@gmail.com>
2021-10-05 22:51:41 -0400

    zypper can use urls directly, yay

--- odeps.sh
+++ odeps.sh
@@ -4,25 +4,23 @@
 echo "installing base dependencies"
 sudo zypper install \
     git \
-	stow \
-	etckeeper \
-	tmux \
-	zsh \
+    stow \
+    etckeeper \
+    tmux \
+    zsh \
     vim \
-	powerline\
-	htop \
-	mc \
-	curl \
+    powerline\
+    htop \
+    mc \
+    curl \
     nnn \
     ack \
     fzf \
     ripgrep \
     grc \
     ncdu \
-    bat \
-    duf \
-    bottom
-#need an exa alternative
+    bat 
+
 read -n1 -p $'Does this system need ssh access?\n' REPLY
 	if [[ $REPLY == [Yy] ]]; then 
 		sudo zypper install openssh-server
@@ -33,4 +31,4 @@ read -n1 -p $'\nDoes this system have a GUI?\n' REPLY
 	else 
 		sudo zypper install emacs-nox
 	fi
-
+sudo zypper install "https://github.com/muesli/duf/releases/download/v0.6.2/duf_0.6.2_linux_amd64.rpm"
