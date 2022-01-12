dc58b1908ae2edbe7a856220c189c946aa96b0aa
Stephen Card <scard@idevops.io>
2022-01-11 23:56:04 -0500

    bash-it installs correctly

--- init.sh
+++ init.sh
@@ -33,6 +33,7 @@ case $answer in
   b | bash)
     echo "Installing Bash-It"
     git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
+    bash ~/.bash_it/install.sh
     ;;
   *)
     echo "Guess we're sticking with vanilla bash then..."
