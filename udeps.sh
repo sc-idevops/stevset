ff5444c5ca53ab6983211db7b2050576178f8488
Stephen Card <scard@idevops.io>
2022-04-18 03:24:31 -0400

    update for jammin jelly! (Ubuntu 22.04LTS)

--- udeps.sh
+++ udeps.sh
@@ -13,7 +13,7 @@ function dpkg_url() {
 
 # installing base dependencies
 sudo apt-get -my install \
-  git \
+	git \
 	stow \
 	etckeeper \
 	tmux \
@@ -23,15 +23,15 @@ sudo apt-get -my install \
 	mc \
 	curl \
 	aptitude \
-  nnn \
-  ack \
-  fzf \
-  ripgrep \
-  grc \
-  fd-find \
-  ncdu \
-  tree \
-  neofetch
+	nnn \
+	ack \
+	fzf \
+	ripgrep \
+	grc \
+	fd-find \
+	ncdu \
+	tree \
+	neofetch
 
 read -n1 -p $'Does this system need a ssh server?\n' REPLY
 	if [[ $REPLY == [Yy] ]]; then 
@@ -43,16 +43,25 @@ read -n1 -p $'\nDoes this system have a GUI?\n' REPLY
 	else
 	  sudo apt-get install emacs-nox
 	fi
-#Fix stupid Rust issue
-if [ -f "/usr/.crates2.json" ]; then
-  sudo sed -i '/crates2/d' /var/lib/dpkg/info/ripgrep.list
-  sudo rm -f /usr/.crates2.json
-  dpkg_url https://github.com/sharkdp/bat/releases/download/v0.20.0/bat_0.20.0_amd64.deb
-  sudo sed -i '/crates2/d' /var/lib/dpkg/info/bat.list
-  sudo rm -f /usr/.crates2.json
+
+release=`lsb_release -cs`
+if [[ $release == jammy ]]; then
+  sudo apt install \
+    bat \
+    duf
+  else
+    dpkg_url https://github.com/muesli/duf/releases/download/v0.8.1/duf_0.8.1_linux_amd64.deb
+    dpkg_url https://github.com/sharkdp/bat/releases/download/v0.20.0/bat_0.20.0_amd64.deb
 fi
 
+#Fix stupid Rust issue
+# if [ -f "/usr/.crates2.json" ]; then
+  # sudo sed -i '/crates2/d' /var/lib/dpkg/info/ripgrep.list
+  # sudo rm -f /usr/.crates2.json
+  # sudo sed -i '/crates2/d' /var/lib/dpkg/info/bat.list
+  # sudo rm -f /usr/.crates2.json
+# fi
+
 #This section installs software outside apt
-dpkg_url https://github.com/muesli/duf/releases/download/v0.8.1/duf_0.8.1_linux_amd64.deb
 dpkg_url https://github.com/ClementTsang/bottom/releases/download/0.6.8/bottom_0.6.8_amd64.deb
 dpkg_url https://github.com/dandavison/delta/releases/download/0.12.1/git-delta_0.12.1_amd64.deb
