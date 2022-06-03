2580172f7ef8a4870311caf37e9a5e808b57f1fd
steveokard <steveokard@gmail.com>
2022-06-03 15:16:42 -0400

    functionalized

--- firefox.sh
+++ firefox.sh
@@ -3,6 +3,7 @@ add_ppa() {
   sudo add-apt-repository ppa:mozillateam/ppa
   sudo apt update
   sudo apt remove firefox
+  echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codename}";' | sudo tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox
 }
 
 #process flags
@@ -13,7 +14,6 @@ while getopts 'efrp:h' opt; do
       add_ppa
       sudo snap remove firefox
       sudo apt install firefox-esr
-      echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codename}";' | sudo tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox
       ;;
 
     f)
@@ -34,7 +34,6 @@ while getopts 'efrp:h' opt; do
 Pin: release o=LP-PPA-mozillateam
 Pin-Priority: 1001
       ' | sudo tee /etc/apt/preferences.d/mozilla-firefox
-      echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codename}";' | sudo tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox
       ;;
 
     p)
