36f0aa361ec1083d7b96c49ab31dabec1cd1116f
steveokard <steveokard@gmail.com>
2022-06-03 15:10:01 -0400

    functionalized

--- firefox.sh
+++ firefox.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
-sudo add-apt-repository ppa:mozillateam/ppa
-sudo apt update
-sudo snap remove firefox
-sudo apt remove firefox
+add_ppa() {
+  sudo add-apt-repository ppa:mozillateam/ppa
+  sudo apt update
+  sudo apt remove firefox
+}
 
 #process flags
 while getopts 'efrp:h' opt; do
   case "$opt" in
     e)
       echo "Installing Firefox ESR"
+      add_ppa
+      sudo snap remove firefox
       sudo apt install firefox-esr
       echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codename}";' | sudo tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox
       ;;
 
     f)
       echo "Installing Flatpak Firefox"
+      sudo snap remove firefox
       sudo apt install flatpak
       flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
       flatpak install flathub org.mozilla.firefox
@@ -22,6 +26,8 @@ while getopts 'efrp:h' opt; do
 
     r)
       echo "Installing Regular Firefox"
+      add_ppa
+      sudo snap remove firefox
       sudo apt install -t 'o=LP-PPA-mozillateam' firefox
       #Pin Priority
       echo 'Package: *
