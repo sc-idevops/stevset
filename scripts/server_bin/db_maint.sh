19182f3d4c1cce5fe7ebb5f986def35b168b6a80
steveokard <steveokard@gmail.com>
2022-11-26 17:46:08 -0500

    line 2 description

--- scripts/server_bin/db_maint.sh
+++ scripts/server_bin/db_maint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#maintenance
+#vaccum the database
 echo "--- DATABASE MAINTENANCE ---"
 cd $HOME/gallery-dl
 sqlite3 archive.sqlite3 'VACUUM;'
--- scripts/server_bin/yc_bak.sh
+++ scripts/server_bin/yc_bak.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+# pull backup from remote server
 rsync -hav --progress --delete --compress yc:/home/stev/backup /home/stev/storage/yc-backup
