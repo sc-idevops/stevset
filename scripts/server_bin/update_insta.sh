cada747e05866500f95079b8ba4674bd764fb11f
steveokard <steveokard@gmail.com>
2022-12-04 15:56:11 -0500

    increase sleep time

--- scripts/server_bin/update_insta.sh
+++ scripts/server_bin/update_insta.sh
@@ -2,7 +2,7 @@
 cd $HOME/gallery-dl/instagram
 for i in */; do
   echo ">> $i"
-  gallery-dl --sleep 2-3 --abort 3 https://www.instagram.com/$i
+  gallery-dl --sleep 3-4 --abort 3 https://www.instagram.com/$i
   sleep 15
 done
 
