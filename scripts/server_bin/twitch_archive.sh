53ae212526cba5fe1578e68d8668802d559d7ed1
steveokard <steveokard@gmail.com>
2022-12-07 00:22:11 -0500

    twitch scripted

---
+++ scripts/server_bin/twitch_archive.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /home/stev/Videos/Twitch/spaceyeen
+yt-dlp -f 720p60 https://www.twitch.tv/spaceyeen/videos
+cd /home/stev/Videos/Twitch/Kodykins
+yt-dlp -f 720p60 https://www.twitch.tv/curlykody/videos
