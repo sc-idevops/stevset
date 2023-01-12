84a140861663c6fea1b84e3d523cf14418c41616
sc-idevops <steveokard@gmail.com>
2023-01-12 02:14:43 -0500

    updated packages

--- udeps.sh
+++ udeps.sh
@@ -14,6 +14,7 @@ function dpkg_url() {
 # installing base dependencies
 sudo apt-get -my install \
 	git \
+  vim \
 	stow \
 	etckeeper \
 	tmux \
@@ -31,7 +32,7 @@ sudo apt-get -my install \
 	fd-find \
 	ncdu \
 	tree \
-	neofetch \
+	neofetch 
   nala
 
 read -n1 -p $'Does this system need a ssh server?\n' REPLY
@@ -53,17 +54,9 @@ if [[ $release == jammy ]]; then
     btop
   else
     dpkg_url https://github.com/muesli/duf/releases/download/v0.8.1/duf_0.8.1_linux_amd64.deb
-    dpkg_url https://github.com/sharkdp/bat/releases/download/v0.20.0/bat_0.20.0_amd64.deb
+    dpkg_url https://github.com/sharkdp/bat/releases/download/v0.22.1/bat_0.22.1_amd64.deb
 fi
 
-#Fix stupid Rust issue
-# if [ -f "/usr/.crates2.json" ]; then
-  # sudo sed -i '/crates2/d' /var/lib/dpkg/info/ripgrep.list
-  # sudo rm -f /usr/.crates2.json
-  # sudo sed -i '/crates2/d' /var/lib/dpkg/info/bat.list
-  # sudo rm -f /usr/.crates2.json
-# fi
-
 #This section installs software outside apt
-dpkg_url https://github.com/ClementTsang/bottom/releases/download/0.6.8/bottom_0.6.8_amd64.deb
-dpkg_url https://github.com/dandavison/delta/releases/download/0.13.0/git-delta_0.13.0_amd64.deb
+dpkg_url https://github.com/ClementTsang/bottom/releases/download/0.7.1/bottom_0.7.1_amd64.deb
+dpkg_url https://github.com/dandavison/delta/releases/download/0.15.1/git-delta_0.15.1_amd64.deb || dpkg_url https://github.com/dandavison/delta/releases/download/0.15.1/git-delta-musl_0.15.1_amd64.deb
