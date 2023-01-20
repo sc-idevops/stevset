1ea17f6d96019826a21489b0c273ca7ff4055d7a
steveokard <steveokard@gmail.com>
2023-01-20 01:31:07 -0500

    removed stray symbol

--- scripts/server_bin/update_mastodon.sh
+++ scripts/server_bin/update_mastodon.sh
@@ -3,7 +3,7 @@ gallery-dl --sleep 12-16 --abort 2 "mastodon:https://yiff.life/@kokirimuscle@mas
 sleep 60
 gallery-dl --sleep 12-16 --abort 2 "mastodon:https://yiff.life/@PupShadowNH@woof.group"
 sleep 60
-gallery-dl --sleep 12-16 --abort 2 "mastodon:https://yiff.life/@KubiBun@meow.social"                            ─┘
+gallery-dl --sleep 12-16 --abort 2 "mastodon:https://yiff.life/@KubiBun@meow.social"
 sleep 60
 gallery-dl --sleep 12-16 --abort 2 "mastodon:https://yiff.life/@Little_LuLu_X@rubber.social"
 sleep 60
