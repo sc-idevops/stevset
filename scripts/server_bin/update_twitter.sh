3add6ee3bd1471149e529b6c411acf4a6464218e
steveokard <steveokard@gmail.com>
2023-01-29 06:16:06 -0500

    increased max wait time

--- scripts/server_bin/update_twitter.sh
+++ scripts/server_bin/update_twitter.sh
@@ -4,12 +4,12 @@ rm ~/twit_error.txt
 cd $HOME/gallery-dl/twitter
 for i in */; do
   echo ">> $i"
-  (gallery-dl --sleep 0-1 --abort 3 "https://twitter.com/$i" || echo "Problem with gallery at https://twitter.com/$i" >> ~/twit_error.txt) &
+  (gallery-dl --sleep 0-2 --abort 3 "https://twitter.com/$i" || echo "Problem with gallery at https://twitter.com/$i" >> ~/twit_error.txt) &
   if [[ $(jobs -r -p | wc -l) -ge $N ]]; then
     wait -n
   fi 
 done
 echo ">>> Updating Retweets"
-gallery-dl --abort 3 --sleep 0-1 -o retweets=true "https://twitter.com/stefenauris"
-gallery-dl --abort 3 --sleep 0-1 -o retweets=true "https://twitter.com/DaybreakHero"
+gallery-dl --abort 3 --sleep 0-2 -o retweets=true "https://twitter.com/stefenauris"
+gallery-dl --abort 3 --sleep 0-2 -o retweets=true "https://twitter.com/DaybreakHero"
 wait
