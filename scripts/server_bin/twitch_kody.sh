d81ad7b2d4f9410f42b8f00f3eae8130ed4c6734
steveokard <steveokard@gmail.com>
2023-02-03 17:44:38 -0500

    added quiet parameter

--- scripts/server_bin/twitch_kody.sh
+++ scripts/server_bin/twitch_kody.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/stev/Videos/Twitch/Kodykins
-yt-dlp -f 720p60 https://www.twitch.tv/curlykody/videos
+yt-dlp -q -f 720p60 https://www.twitch.tv/curlykody/videos
--- scripts/server_bin/twitch_spacey.sh
+++ scripts/server_bin/twitch_spacey.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/stev/Videos/Twitch/spaceyeen
-yt-dlp -f 720p60 https://www.twitch.tv/spaceyeen/videos
+yt-dlp -q -f 720p60 https://www.twitch.tv/spaceyeen/videos
