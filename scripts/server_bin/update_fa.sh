0f39c5fc0b1734f0be775cadd60c6a98de32694f
steveokard <steveokard@gmail.com>
2023-02-16 15:32:25 -0500

    add error reporting

--- scripts/server_bin/update_fa.sh
+++ scripts/server_bin/update_fa.sh
@@ -3,7 +3,7 @@ N=2
 cd $HOME/gallery-dl/furaffinity
 for i in */; do
   echo ">> $i"
-  (gallery-dl --abort 3 --sleep 0-1 https://www.furaffinity.net/user/$i) &
+  (gallery-dl --abort 3 --sleep 0-1 https://www.furaffinity.net/user/$i || echo "$i" >> ~/fa_error.log) &
   if [[ $(jobs -r -p | wc -l) -ge $N ]]; then
     wait -n
   fi 
--- scripts/server_bin/update_twitter.sh
+++ scripts/server_bin/update_twitter.sh
@@ -4,7 +4,7 @@ rm ~/twit_error.txt
 cd $HOME/gallery-dl/twitter
 for i in */; do
   echo ">> $i"
-  (gallery-dl --sleep 0.5-2 --abort 3 "https://twitter.com/$i" || echo "Problem with gallery at https://twitter.com/$i" >> ~/twit_error.txt) &
+  (gallery-dl --sleep 0.8-3 --abort 3 "https://twitter.com/$i" || echo "$i" >> ~/twit_error.txt) &
   if [[ $(jobs -r -p | wc -l) -ge $N ]]; then
     wait -n
   fi 
