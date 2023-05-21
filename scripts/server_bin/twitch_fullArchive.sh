7c606f86c88ca92bc6500177b628253067986f7e
sc-idevops <steveokard@gmail.com>
2023-05-21 00:40:54 -0400

    fixed logic in folder creation

--- scripts/server_bin/twitch_fullArchive.sh
+++ scripts/server_bin/twitch_fullArchive.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-if [[ -z "$2" ]]; then
-  echo "please provide both parameters" 
-  exit 1
-fi
+help()
+{
+  echo "Download all the past broadcasts from a twitch user"
+  echo "Requires 2 Parameters:"
+  echo "    -u the username of the twitch user"
+  echo "    -f the encoding format for the videos, which are fed to yt-dlp"
+}
 while getopts u:f: flag
 do
   case "${flag}" in
@@ -11,15 +14,11 @@ do
     h) help exit;;
   esac
 done
+if [[ -z "$2" ]]; then
+  echo "please provide both parameters" 
+  exit 1
+fi
 
-cd /home/stev/Videos/Twitch/$username || mkdir -v ~/Videos/Twitch/$username
+cd /home/stev/Videos/Twitch/$username || mkdir -v ~/Videos/Twitch/$username && cd ~/Videos/Twitch/$username
 yt-dlp -q -f $format "https://www.twitch.tv/$username/videos?filter=archives&sort=time"
 
-
-help()
-{
-  echo "Download all the past broadcasts from a twitch user"
-  echo "Requires 2 Parameters:"
-  echo "    -u the username of the twitch user"
-  echo "    -f the encoding format for the videos, which are fed to yt-dlp"
-}
