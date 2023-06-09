10b07786bea978c9fa8f8cabd9ea87d8bdc9ff7e
sc-idevops <steveokard@gmail.com>
2023-06-09 00:26:10 -0400

    reconfigured for squishing downloaded movies

--- scripts/server_bin/squishyVid.sh
+++ scripts/server_bin/squishyVid.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-for d in */; do
-  cd $d
+#for d in */; do
+#  cd $d
   for i in *.mp4; do
-    vid_format=$(mediainfo --Inform="Video;%Format%" "$i")
-    #echo video format is $vid_format
-    if [[ "$vid_format" != "HEVC" ]]; then
-      ffmpeg -hide_banner -loglevel error -hwaccel cuda -i "$i" -vcodec libx265 -crf 26 -c:a copy -movflags +faststart -x265-params log-level=0 output.mp4
+      ffmpeg -hide_banner -i "$i" -c:v libvpx-vp9 -c:a libopus -crf 30 -row-mt 1 -cpu-used 2 "$i.mkv"
       trash "$i"
-      mv output.mp4 "$i"
-    fi
+#      mv output.mkv "$i.mkv"
   done
-  cd ..
-done
+#  cd ..
+#done
