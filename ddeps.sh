acfc64974ca5b1a68366d26889ccaf9b14c1c783
StephenC <steveokard@gmail.com>
2023-10-02 00:39:15 -0400

    Update ddeps.sh
    fixed not installing nala
    duf included in default repos now

--- ddeps.sh
+++ ddeps.sh
@@ -35,8 +35,9 @@ sudo apt-get -my install \
   neofetch \
   trash-cli \
   bat \
-  neovim
-  nala
+  neovim \
+  nala \
+  duf
 
 read -n1 -p $'Does this system need a ssh server?\n' REPLY
 	if [[ $REPLY == [Yy] ]]; then 
@@ -50,5 +51,4 @@ read -n1 -p $'\nDoes this system have a GUI?\n' REPLY
 	fi
 
 #This section installs software outside apt
-dpkg_url https://github.com/muesli/duf/releases/download/v0.8.1/duf_0.8.1_linux_amd64.deb
-dpkg_url https://github.com/dandavison/delta/releases/download/0.15.1/git-delta_0.15.1_amd64.deb || dpkg_url https://github.com/dandavison/delta/releases/download/0.15.1/git-delta-musl_0.15.1_amd64.deb
+dpkg_url https://github.com/dandavison/delta/releases/download/0.15.1/git-delta_0.16.5_amd64.deb || dpkg_url https://github.com/dandavison/delta/releases/download/0.16.5/git-delta-musl_0.15.1_amd64.deb
