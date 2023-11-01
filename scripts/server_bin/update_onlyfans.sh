a6291993cdcb882ba7d80da4700175353bd8016c
sc-idevops <steveokard@gmail.com>
2023-11-01 14:51:16 -0400

    not automated but at least it's the command to run

--- scripts/server_bin/update_onlyfans.sh
+++ scripts/server_bin/update_onlyfans.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-cd ~/gallery-dl/onlyfans
-onlyfans-scraper -a 
+docker run  -it --rm --name=ofscraper -u 1000:1000 -v /home/stev/.config/ofscraper:/home/ofscraper/.config/ -v /home/stev/gallery-dl/onlyfans:/home/ofscraper/data ghcr.io/datawhores/of-scraper:latest bash
