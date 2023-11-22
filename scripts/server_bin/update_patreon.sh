4197d47ea989e6db8454bdf63a0326611fc72ddd
sc-idevops <steveokard@gmail.com>
2023-11-22 15:38:14 -0500

    added patreon update script

---
+++ scripts/server_bin/update_patreon.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd $HOME/gallery-dl/patreon/
+gallery-dl --sleep 4-8 --abort 3 "https://www.patreon.com/juiceps"
+gallery-dl --sleep 4-8 --abort 3 "https://www.patreon.com/rackun"
+gallery-dl --sleep 4-8 --abort 3 "https://www.patreon.com/WaddleDox"
+gallery-dl --sleep 4-8 --abort 3 "https://www.patreon.com/Chunie"
+gallery-dl --sleep 4-8 --abort 3 "https://www.patreon.com/KokuKoku"
