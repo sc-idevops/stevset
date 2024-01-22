14db86b04e2fadb9dab8573ee737bb244c08cc5a
sc-idevops <steveokard@gmail.com>
2024-01-21 20:31:31 -0500

    added verbosity

--- scripts/server_bin/fps_cut.sh
+++ scripts/server_bin/fps_cut.sh
@@ -3,8 +3,8 @@
 for i in `fdfind video.mp4`; do
   if [[ (( $(ffprobe -show_streams $i 2>&1 | grep fps | awk '{split($0,a,"fps")}END{print a[1]}' | awk '{print $NF}') > 30 )) ]]; then
     snap run ffmpeg -hide_banner -hwaccel cuda -hwaccel_output_format cuda -i "$i" -c:a copy -c:v h264_nvenc -filter:v fps=30 "${i}_30.mp4" && \
-      rm "$i" ; mv "${i}_30.mp4" "$i"
+      rm --verbose "$i" ; mv --verbose "${i}_30.mp4" "$i"
   fi
-  echo "no edit for $i"
+  echo " << no edit for $i"
 done
 
