c50ccff6c413db809637b73e5d49450110cda1a3
steveokard <steveokard@gmail.com>
2024-02-17 21:07:19 -0500

    refined parsing

--- scripts/server_bin/fps_cut.sh
+++ scripts/server_bin/fps_cut.sh
@@ -2,9 +2,10 @@
 
 for i in `fdfind video.mp4`; do
   if [[ (( $(ffprobe -show_streams $i 2>&1 | grep fps | awk '{split($0,a,"fps")}END{print a[1]}' | awk '{print $NF}') > 30 )) ]]; then
-    snap run ffmpeg -hide_banner -hwaccel cuda -hwaccel_output_format cuda -i "$i" -c:a copy -c:v h264_nvenc -filter:v fps=30 "${i}_30.mp4" && \
-      rm --verbose "$i" ; mv --verbose "${i}_30.mp4" "$i"
+    snap run ffmpeg -hide_banner -hwaccel cuda -hwaccel_output_format cuda -i "$i" -c:a copy -c:v h264_nvenc -filter:v fps=30 "${i}_30.mp4"
+      # && \ rm --verbose "$i" ; mv --verbose "${i}_30.mp4" "$i"
+  else
+    echo " << no edit for $i"
   fi
-  echo " << no edit for $i"
 done
 
