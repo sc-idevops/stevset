d16d20bf3a41492620af58b2c2e9ea8c9d56c813
stephen <stev@noble-testing.lan>
2024-03-02 22:18:17 -0500

    added glow added dust

--- ddeps.sh
+++ ddeps.sh
@@ -50,4 +50,6 @@ read -n1 -p $'\nDoes this system have a GUI?\n' REPLY
 
 #This section installs software outside apt
 dpkg_url https://github.com/dandavison/delta/releases/download/0.16.5/git-delta_0.16.5_amd64.deb
+dpkg_url https://github.com/bootandy/dust/releases/download/v0.9.0/du-dust_0.9.0-1_amd64.deb
+dpkg_url https://github.com/charmbracelet/glow/releases/download/v1.5.1/glow_1.5.1_amd64.deb
 
