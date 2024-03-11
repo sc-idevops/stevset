65a82c8d17b65e9d8efb3fd8313f49080d0265bd
sc-idevops <steveokard@gmail.com>
2024-03-11 01:19:55 -0400

    new stable version

--- scripts/server_bin/update_onlyfans.sh
+++ scripts/server_bin/update_onlyfans.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # docker pull ghcr.io/datawhores/of-scraper:latest
-docker run  -it --rm --name=ofscraper -u 1000:1000 -v /home/stev/.config/ofscraper:/home/ofscraper/.config/ -v /home/stev/gallery-dl/onlyfans:/home/ofscraper/data ghcr.io/datawhores/of-scraper:3.7.1 ofscraper
+docker run  -it --rm --name=ofscraper -u 1000:1000 -v /home/stev/.config/ofscraper:/home/ofscraper/.config/ -v /home/stev/gallery-dl/onlyfans:/home/ofscraper/data ghcr.io/datawhores/of-scraper:3.8.5 ofscraper
