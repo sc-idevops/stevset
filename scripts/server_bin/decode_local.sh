625cabd5416e5e732a09c4ba475e55bb9ea4b314
sc-idevops <steveokard@gmail.com>
2024-03-10 20:29:42 -0400

    safe mode on

--- scripts/server_bin/decode_local.sh
+++ scripts/server_bin/decode_local.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 for i in `fdfind -d=1 --extension=mp4`; do
-  snap run ffmpeg -hide_banner -hwaccel cuda -hwaccel_output_format cuda -i "$i" -c:a copy -c:v h264_nvenc "${i}_30.mp4" && \     
-  rm --verbose "$i" ; mv --verbose "${i}_30.mp4" "$i"
+  snap run ffmpeg -hide_banner -hwaccel cuda -hwaccel_output_format cuda -i "$i" -c:a copy -c:v h264_nvenc "${i}_30.mp4" 
+  #safe mode on
+  #rm --verbose "$i" ; mv --verbose "${i}_30.mp4" "$i"
 done
