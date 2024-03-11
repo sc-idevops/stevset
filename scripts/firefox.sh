265c69a09c3a5b22ecfde92147880927696cfa49
stephen <stev@noble-testing.lan>
2024-03-10 20:25:22 -0400

    updated with new mozilla repo

--- scripts/firefox.sh
+++ scripts/firefox.sh
@@ -3,16 +3,9 @@ help() {
       echo "Usage: $(basename $0) 
       [-e] Installs Firefox ESR 
       [-f] Installs Flatpak Firefox
-      [-d] Installs Firefox from the PPA and Pins it's Priority in apt
+      [-d] Installs Firefox from the official Mozilla Repo and Pins it's Priority in apt
       [-p] Purges the Snapd daemon from the system. Use if you don't want snaps anymore period."
       exit 1
-}
-add_ppa() {
-  sudo add-apt-repository ppa:mozillateam/ppa
-  sudo apt-get update
-  sudo apt-get -y remove firefox
-  sudo snap remove firefox
-  echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codename}";' | sudo tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox
 }
 
 if [ -z "$*" ]; then help; fi
@@ -22,32 +15,45 @@ while getopts 'efrp:h' opt; do
   case "$opt" in
     e)
       echo "Installing Firefox ESR"
-      add_ppa
+      sudo add-apt-repository ppa:mozillateam/ppa
+      sudo apt-get update
+      sudo apt-get -y remove firefox
+      sudo snap remove firefox
+      echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codename}";' | sudo tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox
       sudo apt-get -y install firefox-esr
       ;;
 
     f)
       echo "Installing Flatpak Firefox"
-      sudo snap remove firefox
       sudo apt-get -y install flatpak
       flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
       flatpak install flathub org.mozilla.firefox
       ;;
 
     d)
-      echo "Installing Regular Firefox"
-      add_ppa
-      sudo apt-get -y install -t 'o=LP-PPA-mozillateam' firefox
-      #Pin Priority
-      echo 'Package: *
-Pin: release o=LP-PPA-mozillateam
-Pin-Priority: 1001
-      ' | sudo tee /etc/apt/preferences.d/mozilla-firefox
+      echo "Installing Firefox from Offical Mozilla Repo"
+      #Create an APT keyring (if one doesn’t already exist):
+      sudo install -d -m 0755 /etc/apt/keyrings
+      wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
+      echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
+      echo '
+Package: *
+Pin: origin packages.mozilla.org
+Pin-Priority: 1000
+      ' | sudo tee /etc/apt/preferences.d/mozilla
+      sudo apt update && sudo apt install firefox
       ;;
 
     p)
       echo "Purging snapd..."
       sudo apt-get autoremove snapd
+      ;;
+
+    r)
+      echo "Purging Firefox Snap"
+      echo "Backing up Snap Profile"
+      cp -r ~/snap/firefox/current ~/mozilla_backup
+      sudo snap remove firefox
       ;;
 
     *)
