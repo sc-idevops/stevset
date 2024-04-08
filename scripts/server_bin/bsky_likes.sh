4ce1bd2c6251697ac4c18d6ea4564c0f20ff23f1
sc-idevops <steveokard@gmail.com>
2024-04-08 17:17:11 -0400

    fixed script

--- scripts/server_bin/bsky_likes.sh
+++ scripts/server_bin/bsky_likes.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gallery-dl -u steveokard@gmail.com -p `cat ~/.gallery-dl/bsky-secret.txt` --abort 3 --sleep 1-2 -o include=likes -o archive=~/gallery-dl/bluesky/stefenauris.com/archive.sqlite3 -d ~/gallery-dl/bluesky/stefenauris.com/ "https://bsky.app/profile/stefenauris.com"
+gallery-dl -u stefenauris.com --abort 3 --sleep 1-2 -o include=all -o reposts=true -o archive=~/gallery-dl/bluesky/stefenauris.com/likes.sqlite3 -o blacklist= -d ~/gallery-dl/bluesky/stefenauris.com/ "https://bsky.app/profile/stefenauris.com"
