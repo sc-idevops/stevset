00ef018d67d83ba6f8eaa627de3805d81da4ea54
sc-idevops <steveokard@gmail.com>
2024-06-09 20:50:47 -0400

    docker image update

--- scripts/server_bin/update_onlyfans.sh
+++ scripts/server_bin/update_onlyfans.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # docker pull ghcr.io/datawhores/of-scraper:latest
 docker run -it --rm --name=ofscraper -u 1000:1000 -v /home/stev/.config/ofscraper:/home/ofscraper/.config/ -v /home/stev/gallery-dl/onlyfans:/home/ofscraper/data \
-  ghcr.io/datawhores/of-scraper:3.9.1 \
+  ghcr.io/datawhores/of-scraper:3.10.6 \
   ofscraper --output normal --action download --sort subscribed --post timeline,profile,messages,purchased -ts --username ALL
