9a7204692f66148082553f8bdd4d8701d39523d5
sc-idevops <steveokard@gmail.com>
2024-06-11 02:03:06 -0400

    from twitter to x domain

--- scripts/server_bin/twitt_follow.sh
+++ scripts/server_bin/twitt_follow.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 target=/home/stev/Pictures/gallery-dl/twitter/follow.txt
 #edit follows file in place
-sed -n -i '/^@/p' $target
-sed -ie 's/^.//' $target
+#sed -n -i '/^@/p' $target
+#sed -ie 's/^.//' $target
 #loop through the list and download
 for i in `cat $target`; do
-  gallery-dl --sleep 4 --abort 3 "https://twitter.com/$i"
+  gallery-dl --sleep 4 --abort 3 "https://x.com/$i"
 done
 
