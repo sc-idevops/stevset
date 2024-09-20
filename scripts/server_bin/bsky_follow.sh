c08ec192dfa2e9bec6d807cbebe198cd9f22b2bb
sc-idevops <steveokard@gmail.com>
2024-09-20 00:49:53 -0400

    addressed unicode character problem

--- scripts/server_bin/bsky_follow.sh
+++ scripts/server_bin/bsky_follow.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-target=/home/stev/Pictures/gallery-dl/bluesky/follow.txt
+target=/home/stev/Pictures/gallery-dl/bluesky/follow2.txt
 #edit follows file in place
+iconv -c -f utf-8 -t ascii /home/stev/Pictures/gallery-dl/bluesky/follow.txt > $target
 sed -n -i '/^@/p' $target
 sed -ie 's/^.//' $target
 #loop through the list and download
