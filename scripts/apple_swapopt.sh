c3da9e3a321ddb191582fc27a3e5bcf51ca32bcd
sc-idevops <stefenauris@proton.me>
2024-11-22 21:50:29 -0500

    added line to enable it now

--- scripts/apple_swapopt.sh
+++ scripts/apple_swapopt.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # https://askubuntu.com/a/919096
+echo 1 | sudo tee /sys/module/hid_apple/parameters/swap_opt_cmd
 echo options hid_apple swap_opt_cmd=1 | sudo tee -a /etc/modprobe.d/hid_apple.conf
 sudo update-initramfs -u -k all
