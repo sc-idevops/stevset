d797d95cb3c30114a66f77dd64c7b484c0c1602f
sc-idevops <stefenauris@proton.me>
2025-01-08 12:43:08 -0500

    fixed package priority

--- scripts/firefox.sh
+++ scripts/firefox.sh
@@ -35,6 +35,10 @@ help() {
 Package: *
 Pin: origin packages.mozilla.org
 Pin-Priority: 1000
+
+Package: firefox*
+Pin: release o=Ubuntu
+Pin-Priority: -1
       ' | sudo tee /etc/apt/preferences.d/mozilla
       sudo apt update && sudo apt install firefox
       ;;
