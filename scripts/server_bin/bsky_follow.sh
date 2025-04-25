8e42abcad42bf85d8ce4a37a9ba34f0f9aa29360
sc-idevops <steveokard@gmail.com>
2025-04-25 13:00:12 -0400

    check if list is valid before proceeding

--- scripts/server_bin/bsky_follow.sh
+++ scripts/server_bin/bsky_follow.sh
@@ -4,8 +4,12 @@ target=/home/stev/Pictures/gallery-dl/bluesky/follow2.txt
 iconv -c -f utf-8 -t ascii /home/stev/Pictures/gallery-dl/bluesky/follow.txt > $target
 sed -n -i '/^@/p' $target
 sed -ie 's/^.//' $target
+#check if list is valid
+cat $target
+read -e -p "is this list valid? " valid
+if [[ "$valid"  == [Yy]* ]] 
 #loop through the list and download
 for i in `cat $target`; do
   gallery-dl --sleep 1-3 "https://bsky.app/profile/$i"
 done
-
+fi
