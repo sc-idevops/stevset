62da0cd6daa060da98079f121f5226846c134f54
sc-idevops <steveokard@gmail.com>
2025-06-07 22:14:47 -0400

    RIP spacevim

--- spacevim.sh
+++ spacevim.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
 echo "Installing Spacevim!"
-[ -d $HOME/.vim ] && mv "$HOME/.vim" "$HOME/vim_bk"
-[ -f $HOME/.vimrc ] && mv "$HOME/.vimrc" "$HOME/vimrc_bk"
-curl -sLf https://spacevim.org/install.sh | bash -s -- --install vim
 
 echo "Install Neovim"
-trash ~/.local/share/nvim ~/.config/nvim #removes spacevim's symlink
 mkdir -v ~/bin
-if [[ $(lsb_release -cs) != noble ]]; then
-  trash ~/bin/nvim 
-  curl -L https://github.com/neovim/neovim/releases/latest/download/nvim.appimage -o ~/bin/nvim
-  chmod u+x ~/bin/nvim
-else
-  sudo apt install neovim
-fi
+trash ~/bin/nvim 
+curl -L https://github.com/neovim/neovim/releases/download/v0.11.2/nvim-linux-x86_64.appimage -o ~/bin/nvim
+chmod u+x ~/bin/nvim
 
 echo "Install NvChad"
-git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
+git clone https://github.com/NvChad/starter ~/.config/nvim
 
 #echo "Install Lunar Vim"
 #source: https://www.lunarvim.org/docs/installation
