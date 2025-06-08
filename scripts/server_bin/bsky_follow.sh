2ed701736be28b50915664039e965f0eefcc9e6f
sc-idevops <steveokard@gmail.com>
2025-06-07 22:38:36 -0400

    script updates

--- scripts/server_bin/bsky_follow.sh
+++ scripts/server_bin/bsky_follow.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 target=/home/stev/Pictures/gallery-dl/bluesky/follow2.txt
 #edit follows file in place
-iconv -c -f utf-8 -t ascii /home/stev/Pictures/gallery-dl/bluesky/follow.txt > $target
-sed -n -i '/^@/p' $target
-sed -ie 's/^.//' $target
+read -e -p "edit the file $target ? " edit
+if [[ "$edit"  == [Yy]* ]] 
+  iconv -c -f utf-8 -t ascii /home/stev/Pictures/gallery-dl/bluesky/follow.txt > $target
+  sed -n -i '/^@/p' $target
+  sed -ie 's/^.//' $target
+fi
 #check if list is valid
 cat $target
 read -e -p "is this list valid? " valid
 if [[ "$valid"  == [Yy]* ]] 
-#loop through the list and download
-for i in `cat $target`; do
-  gallery-dl --sleep 1-3 "https://bsky.app/profile/$i"
-done
+  #loop through the list and download
+  for i in `cat $target`; do
+    gallery-dl --sleep 1-3 "https://bsky.app/profile/$i"
+  done
 fi
