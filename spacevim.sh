1e12de316b446c72ae561bfdb8a7ac9d5c78839f
steveokard <steveokard@gmail.com>
2025-06-07 22:35:35 -0400

    further cleanup before install

--- spacevim.sh
+++ spacevim.sh
@@ -8,8 +8,12 @@ curl -L https://github.com/neovim/neovim/releases/download/v0.11.2/nvim-linux-x8
 chmod u+x ~/bin/nvim
 
 echo "Install NvChad"
+# https://nvchad.com/docs/quickstart/install
+trash ~/.config/nvim/
+trash ~/.local/state/nvim/
+trash ~/.local/share/nvim/
 git clone https://github.com/NvChad/starter ~/.config/nvim
 
 #echo "Install Lunar Vim"
 #source: https://www.lunarvim.org/docs/installation
-#LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
+#LV_BRANCH='release-1.4/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.4/neovim-0.9/utils/installer/install.sh)
