17bbd6ef4e42094ecb53734c735bfe46018c698f
sc-idevops <steveokard@gmail.com>
2025-07-06 23:00:27 -0400

    updates

--- scripts/server_bin/bsky_follow.sh
+++ scripts/server_bin/bsky_follow.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 target=/home/stev/Pictures/gallery-dl/bluesky/follow2.txt
 #edit follows file in place
-read -e -p "edit the file $target ? " edit
-if [[ "$edit"  == [Yy]* ]] 
+read -e -p "edit the file? " edit
+if [[ "$edit"  == [Yy]* ]]; then 
   iconv -c -f utf-8 -t ascii /home/stev/Pictures/gallery-dl/bluesky/follow.txt > $target
   sed -n -i '/^@/p' $target
   sed -ie 's/^.//' $target
@@ -10,7 +10,7 @@ fi
 #check if list is valid
 cat $target
 read -e -p "is this list valid? " valid
-if [[ "$valid"  == [Yy]* ]] 
+if [[ "$valid"  == [Yy]* ]]; then 
   #loop through the list and download
   for i in `cat $target`; do
     gallery-dl --sleep 1-3 "https://bsky.app/profile/$i"
--- scripts/server_bin/update_fa.sh
+++ scripts/server_bin/update_fa.sh
@@ -2,9 +2,6 @@
 N=2
 trash ~/fa_error.log
 cd $HOME/gallery-dl/furaffinity
-gallery-dl --abort 3 --sleep 0-1 "https://www.furaffinity.net/favorites/stevo/"
-gallery-dl --abort 3 --sleep 0-1 "https://www.furaffinity.net/favorites/yrrufaton07/"
-gallery-dl --abort 3 --sleep 0-1 "https://www.furaffinity.net/favorites/arandomguyontheinternet/"
 
 for i in */; do
   echo ">> $i"
@@ -14,3 +11,6 @@ for i in */; do
   fi 
 done
 wait
+gallery-dl --abort 3 --sleep 0-1 "https://www.furaffinity.net/favorites/stevo/"
+gallery-dl --abort 3 --sleep 0-1 "https://www.furaffinity.net/favorites/yrrufaton07/"
+gallery-dl --abort 3 --sleep 0-1 "https://www.furaffinity.net/favorites/arandomguyontheinternet/"
