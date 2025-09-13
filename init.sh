85ab4b6827d8ed5f335e9179e477018fa6cb9d27
sc-idevops <steveokard@gmail.com>
2025-09-13 03:45:01 -0400

    use flag to indicate whether to destroy current nvim config or not

--- init.sh
+++ init.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 #install pre-req.
-echo -n "Which Linux flavor will we be installing programs for? (u/o/a/f/d)"; read -r answer
+echo -n "Which Linux flavor will we be installing programs for? (u/o/a/f/d)"
+read -r answer
 case $answer in
-  u) 
-    bash udeps.sh
-    ;;
-  o)
-    bash odeps.sh
-    ;;
-  a)
-    bash adeps.sh
-    ;;
-  f)
-    bash fdeps.sh
-    ;;
-  d)
-    bash ddeps.sh
-    ;;
-  *)
-    echo "Skipping installation of dependencies!"	
-    ;;
+u)
+  bash udeps.sh
+  ;;
+o)
+  bash odeps.sh
+  ;;
+a)
+  bash adeps.sh
+  ;;
+f)
+  bash fdeps.sh
+  ;;
+d)
+  bash ddeps.sh
+  ;;
+*)
+  echo "Skipping installation of dependencies!"
+  ;;
 esac
 
 #create symlinks using stow
@@ -31,32 +32,33 @@ chmod -R 0600 ~/.ssh/*
 
 #setup shell
 #stow home <-- what is this for??
-echo "Select which shell to configure and use: zsh/fish/bash"; read -r answer
+echo "Select which shell to configure and use: zsh/fish/bash"
+read -r answer
 case $answer in
-  z | zsh)
-    echo "Initializing Prezto"
-    zsh prezto.zsh
-    echo "Enter user password to change shell"
-    chsh -s /usr/bin/zsh 
-    ;;
-  f | fish)
-    echo "Installing Oh-My-Fish"
-    bash fish.sh
-    echo "Enter user password to change shell"
-    chsh -s /usr/bin/fish
-    ;;
-  b | bash)
-    echo "Installing Bash-It"
-    git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
-    bash ~/.bash_it/install.sh
-    ;;
-  *)
-    echo "Guess we're sticking with vanilla bash then..."
-    #inject source line into bashrc
-    if ! grep -q ".sensible.bash" "$HOME/.bashrc"; then
-       echo "source \"\$HOME/.sensible.bash\"" >> "$HOME/.bashrc"
-    fi
-    ;;
+z | zsh)
+  echo "Initializing Prezto"
+  zsh prezto.zsh
+  echo "Enter user password to change shell"
+  chsh -s /usr/bin/zsh
+  ;;
+f | fish)
+  echo "Installing Oh-My-Fish"
+  bash fish.sh
+  echo "Enter user password to change shell"
+  chsh -s /usr/bin/fish
+  ;;
+b | bash)
+  echo "Installing Bash-It"
+  git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
+  bash ~/.bash_it/install.sh
+  ;;
+*)
+  echo "Guess we're sticking with vanilla bash then..."
+  #inject source line into bashrc
+  if ! grep -q ".sensible.bash" "$HOME/.bashrc"; then
+    echo "source \"\$HOME/.sensible.bash\"" >>"$HOME/.bashrc"
+  fi
+  ;;
 esac
 
 #init TMP
@@ -64,29 +66,32 @@ echo "set up Tmux Plugins"
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 #Configure git user
-git_user () {
-  echo -n "Would you like to configure your git name and email? (y/n) => "; read -r answer
+git_user() {
+  echo -n "Would you like to configure your git name and email? (y/n) => "
+  read -r answer
   if [[ $answer = "Y" ]] || [[ $answer = "y" ]]; then
-    echo -n "What is your git user name => "; read -r name
+    echo -n "What is your git user name => "
+    read -r name
     git config --global user.name "$name"
-    echo -n "What is your git email => "; read -r email
+    echo -n "What is your git email => "
+    read -r email
     git config --global user.email "$email"
   fi
 }
 
-#install neovim & nvchad
-echo "Installing neovim & Friends!"
-bash ./lazyvim.sh
+#install neovim & lazyvim
+echo "Installing lazyvim!"
+bash ./lazyvim.sh -c
 
 #install fonts for terminal
-fonts () {
+fonts() {
   echo "installing fonts"
   mkdir ~/.fonts
   wget -q https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/SourceCodePro.tar.xz -O - | tar -xJf - -C ~/.fonts
   wget -q https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/UbuntuMono.tar.xz -O - | tar -xJf - -C ~/.fonts
 }
 
-flat_timer () {
+flat_timer() {
   #this copies and activates a systemd service to enable automatic flatpak updates
   mkdir -pv $HOME/.config/systemd/user/
   cp ./scripts/systemd/flatpak* $HOME/.config/systemd/user/
@@ -96,12 +101,13 @@ flat_timer () {
 # Functions
 git_user
 fonts
-if command -v flatpak &> /dev/null; then
+if command -v flatpak &>/dev/null; then
   flat_timer
 fi
 
-#script to link to stevserver over LAN. Note: the username in the filename and file have to match yours. 
-echo -n "Will you need to connect to stev-server?"; read -r answer
+#script to link to stevserver over LAN. Note: the username in the filename and file have to match yours.
+echo -n "Will you need to connect to stev-server?"
+read -r answer
 if [[ $answer == [Yy] ]]; then
   sudo apt install nfs-common
   sudo cp scripts/systemd/home-stev-server.mount /etc/systemd/system
@@ -112,4 +118,3 @@ fi
 echo "*******************************"
 echo "*    Restart your terminal    *"
 echo "*******************************"
-
--- lazyvim.sh
+++ lazyvim.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
-echo "Setting up Vim Sensible"
-trash ~/.vim/
-mkdir -p ~/.vim/pack/tpope/start
-git clone https://tpope.io/vim/sensible.git ~/.vim/pack/tpope/start
 
-echo "Install Neovim"
+## If passed the c flag, trash the current config
+while getopts ":c" option; do
+  case $option in
+  c)
+    echo "Clearing configurations"
+    trash ~/.vim/
+    mkdir -p ~/.vim/pack/tpope/start
+    git clone https://tpope.io/vim/sensible.git ~/.vim/pack/tpope/start
+    trash ~/.config/nvim/
+    trash ~/.local/state/nvim/
+    trash ~/.local/share/nvim/
+    trash ~/.cache/nvim
+    ;;
+  esac
+done
+
+echo "Install/Update Neovim & Neovide"
 mkdir -v ~/bin
-if [[ ! -f "/usr/bin/neovim" ]]; then
-  trash ~/bin/nvim
-  curl -L https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.appimage -o ~/bin/nvim
-  chmod u+x ~/bin/nvim
-  curl -L https://github.com/neovide/neovide/releases/latest/download/neovide.AppImage -o ~/bin/neovide
-  chmod u+x ~/bin/neovide
-fi
+trash ~/bin/nvim
+curl -L https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.appimage -o ~/bin/nvim
+chmod u+x ~/bin/nvim
+curl -L https://github.com/neovide/neovide/releases/latest/download/neovide.AppImage -o ~/bin/neovide
+chmod u+x ~/bin/neovide
 
 echo "Install Lazyvim"
 # https://www.lazyvim.org/installation
-trash ~/.config/nvim/
-trash ~/.local/state/nvim/
-trash ~/.local/share/nvim/
-trash ~/.cache/nvim
-git clone https://github.com/LazyVim/starter ~/.config/nvim
+if [ ! -d "~/.config/nvim" ]; then
+  git clone https://github.com/LazyVim/starter ~/.config/nvim
+fi
