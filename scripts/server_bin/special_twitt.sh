1a7ee4db7d676f151df5764d15afb654a38f6261
sc-idevops <steveokard@gmail.com>
2025-09-15 02:07:42 -0400

    updates

--- scripts/server_bin/special_twitt.sh
+++ scripts/server_bin/special_twitt.sh
@@ -3,53 +3,62 @@ echo ">>> Updating Retweets"
 gallery-dl --abort 3 --sleep 4 -o retweets=true "https://x.com/stefenauris"
 gallery-dl --abort 3 --sleep 4 -o retweets=true "https://x.com/DaybreakHero"
 echo ">>> Downloading Special Pics in no particular order..."
-gallery-dl --sleep 4 --abort 3 https://x.com/Metal_AnimalMan
-gallery-dl --sleep 4 --abort 3 https://x.com/wolf_ezo
-gallery-dl --sleep 4 --abort 3 https://x.com/_maxsmall
-gallery-dl --sleep 4 --abort 3 https://x.com/ronnoyeen
-gallery-dl --sleep 4 --abort 3 https://x.com/Calilum_
-gallery-dl --sleep 4 --abort 3 https://x.com/sciopup
-gallery-dl --sleep 4 --abort 3 https://x.com/DanielsWolf1
-gallery-dl --sleep 4 --abort 3 https://x.com/lobogriswrestle
-gallery-dl --sleep 4 --abort 3 https://x.com/cheetah_paws
-gallery-dl --sleep 4 --abort 3 https://x.com/K1ngYeen
-gallery-dl --sleep 4 --abort 3 https://x.com/BakedBaxxie
-gallery-dl --sleep 4 --abort 3 https://x.com/angel_pubby
-gallery-dl --sleep 4 --abort 3 https://x.com/AgonyLight
-gallery-dl --sleep 4 --abort 3 https://x.com/AFoxyGent
-gallery-dl --sleep 4 --abort 3 https://x.com/PupShade
-gallery-dl --sleep 4 --abort 3 https://x.com/LukaBuck
-gallery-dl --sleep 4 --abort 3 https://x.com/Wolfy_WolFang
-gallery-dl --sleep 4 --abort 3 https://x.com/WolfSandman
-gallery-dl --sleep 4 --abort 3 https://x.com/Wolf_ODaddy
-gallery-dl --sleep 4 --abort 3 https://x.com/WeaselZERO
-gallery-dl --sleep 4 --abort 3 https://x.com/WeaselAfterDark
-gallery-dl --sleep 4 --abort 3 https://x.com/VectorShen
-gallery-dl --sleep 4 --abort 3 https://x.com/Tworld99
-gallery-dl --sleep 4 --abort 3 https://x.com/TJwolfPupAD
-gallery-dl --sleep 4 --abort 3 https://x.com/theonlyinleb
-gallery-dl --sleep 4 --abort 3 https://x.com/The_Huggernaut
-gallery-dl --sleep 4 --abort 3 https://x.com/TehBasco
-gallery-dl --sleep 4 --abort 3 https://x.com/SwiftLPRD
-gallery-dl --sleep 4 --abort 3 https://x.com/himbohyena
-gallery-dl --sleep 4 --abort 3 https://x.com/Duke_Doberman
-gallery-dl --sleep 4 --abort 3 https://x.com/rubberbeefcake
-gallery-dl --sleep 4 --abort 3 https://x.com/AlienSymbol
-gallery-dl --sleep 4 --abort 3 https://x.com/Milesfuzz
-gallery-dl --sleep 4 --abort 3 https://x.com/yamasmut
-gallery-dl --sleep 4 --abort 3 https://x.com/lucthelab
-gallery-dl --sleep 4 --abort 3 https://x.com/HYDROKIINESIS
-gallery-dl --sleep 4 --abort 3 https://x.com/puppanic
-gallery-dl --sleep 4 --abort 3 https://x.com/t_kobs
-gallery-dl --sleep 4 --abort 3 https://x.com/PupBuday
-gallery-dl --sleep 4 --abort 3 https://x.com/ThunderMaxy
-gallery-dl --sleep 4 --abort 3 https://x.com/FrayeVfraye
-gallery-dl --sleep 4 --abort 3 https://x.com/FullPurp
-gallery-dl --sleep 4 --abort 3 https://x.com/growstav
-gallery-dl --sleep 4 --abort 3 https://x.com/Pup_Krypto
-gallery-dl --sleep 4 --abort 3 https://x.com/PupPanic
-gallery-dl --sleep 4 --abort 3 https://x.com/DoggoStahl
-gallery-dl --sleep 4 --abort 3 https://x.com/AidanEnormouse
-gallery-dl --sleep 4 --abort 3 https://x.com/SmileyBots
-gallery-dl --sleep 4 --abort 3 https://x.com/Aquafighter_Dai
-gallery-dl --sleep 4 --abort 3 https://x.com/acethefurri
+array=(
+  acethefurri
+  AFoxyGent
+  AgonyLight
+  AidanEnormouse
+  AlienSymbol
+  angel_pubby
+  Aquafighter_Dai
+  BakedBaxxie
+  Calilum_
+  cheetah_paws
+  DanielsWolf1
+  DoggoStahl
+  Duke_Doberman
+  FrayeVfraye
+  FullPurp
+  growstav
+  himbohyena
+  HYDROKIINESIS
+  Joshontwt
+  K1ngYeen
+  lobogriswrestle
+  lucthelab
+  LukaBuck
+  _maxsmall
+  Metal_AnimalMan
+  Milesfuzz
+  PalaceWolf_
+  pec_inspector
+  PupBuday
+  Pup_Krypto
+  PupPanic
+  PupShade
+  pupzanderwoodz
+  RingerShep
+  ronnoyeen
+  rubberbeefcake
+  sciopup
+  SmileyBots
+  SwiftLPRD
+  TehBasco
+  The_Huggernaut
+  theonlyinleb
+  ThunderMaxy
+  TJwolfPupAD
+  t_kobs
+  Tworld99
+  VectorShen
+  WeaselAfterDark
+  WeaselZERO
+  wolf_ezo
+  Wolf_ODaddy
+  WolfSandman
+  Wolfy_WolFang
+  yamasmut
+)
+for i in "${array[@]}"; do
+  gallery-dl --sleep 4 --abort 3 https://x.com/"$i"
+done
--- scripts/server_bin/update_fa.sh
+++ scripts/server_bin/update_fa.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 N=2
-trash ~/fa_error.log
+trash ~/fa_error.log 2>/dev/null
 cd $HOME/gallery-dl/furaffinity
 
 for i in */; do
   echo ">> $i"
-  (gallery-dl --abort 3 --sleep 0-1 https://www.furaffinity.net/user/$i || echo "$i" >> ~/fa_error.log) &
+  (gallery-dl --abort 3 --sleep 0-2 https://www.furaffinity.net/user/$i || echo "$i" >>~/fa_error.log) &
   if [[ $(jobs -r -p | wc -l) -ge $N ]]; then
     wait -n
-  fi 
+  fi
 done
 wait
-gallery-dl --abort 3 --sleep 0-1 "https://www.furaffinity.net/favorites/stevo/"
-gallery-dl --abort 3 --sleep 0-1 "https://www.furaffinity.net/favorites/yrrufaton07/"
-gallery-dl --abort 3 --sleep 0-1 "https://www.furaffinity.net/favorites/arandomguyontheinternet/"
+gallery-dl --abort 3 --sleep 0-2 "https://www.furaffinity.net/favorites/stevo/"
+gallery-dl --abort 3 --sleep 0-2 "https://www.furaffinity.net/favorites/yrrufaton07/"
+gallery-dl --abort 3 --sleep 0-2 "https://www.furaffinity.net/favorites/arandomguyontheinternet/"
