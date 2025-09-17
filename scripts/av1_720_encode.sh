2f3155c1321607b8a0e87c3335385a3c700a511b
sc-idevops <stefenauris@proton.me>
2025-09-16 22:58:04 -0400

    for when you need to downgrade something

---
+++ scripts/av1_720_encode.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#this script assumes its being run in the working directory
+
+#test to see if its already been encoded to av1 or else it converts it
+format=$(ffprobe -v error -select_streams v:0 -show_entries stream=codec_name -of default=nokey=1:noprint_wrappers=1 "$1")
+rate=26
+echo -e "\033[0;36m >> Format of file $1 is  ---> $format \033[0m"
+
+if [ "$format" != "av1" ]; then
+  docker run -it --rm --device=/dev/dri:/dev/dri -v "$(pwd)":/config linuxserver/ffmpeg \
+    -hide_banner \
+    -hwaccel qsv -hwaccel_output_format qsv -qsv_device /dev/dri/renderD128 \
+    -n \
+    -i "/config/$1" \
+    -vf "scale_qsv=-1:720" \
+    -c:v av1_qsv \
+    -strict 1 \
+    -global_quality $rate \
+    -look_ahead 1 -look_ahead_depth 100 \
+    -preset 3 \
+    -g 150 \
+    -c:a aac \
+    "/config/${1%.*}_converted.mkv" #    -hide_banner -loglevel warning -v quiet -stats \
+fi
