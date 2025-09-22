459a511e3830b0974c62f4ad522db39ba41096b4
steveokard <steveokard@gmail.com>
2025-09-21 21:04:17 -0400

    add bazzite deps

---
+++ baz_deps.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#one advantage here is that everything cli related can be managed by brew without needing any external help!
+brew install \
+  ack \
+  bat \
+  curl \
+  duf \
+  dust \
+  fd \
+  git-delta \
+  glow \
+  grc \
+  htop \
+  lua \
+  luarocks \
+  make \
+  mc \
+  ncdu \
+  neovim \
+  nnn \
+  ripgrep \
+  shellcheck \
+  stow \
+  tmux \
+  trash-cli \
+  tree \
+  wget \
+  zsh
