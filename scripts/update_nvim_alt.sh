25b510ad0908c81de45fc3a0bdde7c6acd3d9da1
steveokard <steveokard@gmail.com>
2025-09-27 23:03:09 -0400

    quick script to make nvim default editor

---
+++ scripts/update_nvim_alt.sh
@@ -0,0 +1,8 @@
+CUSTOM_NVIM_PATH=$HOME/bin/nvim
+# Set the above with the correct path, then run the rest of the commands:
+set -u
+sudo update-alternatives --install /usr/bin/ex ex "${CUSTOM_NVIM_PATH}" 110
+sudo update-alternatives --install /usr/bin/vi vi "${CUSTOM_NVIM_PATH}" 110
+sudo update-alternatives --install /usr/bin/view view "${CUSTOM_NVIM_PATH}" 110
+sudo update-alternatives --install /usr/bin/vim vim "${CUSTOM_NVIM_PATH}" 110
+sudo update-alternatives --install /usr/bin/vimdiff vimdiff "${CUSTOM_NVIM_PATH}" 110
