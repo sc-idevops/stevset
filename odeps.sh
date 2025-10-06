a25d1997cc48780cf2543ff881d9327c28764283
steveokard <steveokard@gmail.com>
2025-10-06 02:13:56 -0400

    alphabetized

--- odeps.sh
+++ odeps.sh
@@ -3,38 +3,56 @@
 
 echo "installing base dependencies"
 sudo zypper install \
-    git \
-    vim \
-    stow \
-    etckeeper \
-    tmux \
-    zsh \
-    powerline\
-    fd \
-    htop \
-    mc \
-    curl \
-    nnn \
-    ack \
-    fzf \
-    ripgrep \
-    grc \
-    ncdu \
-    bat \
-    trash-cli \
-    lua54 \
-    duf \
-    dust \
-    neovim
+  ack \
+  curl \
+  duf \
+  etckeeper \
+  fzf \
+  git \
+  git-delta \
+  grc \
+  htop \
+  lua51 \
+  lua51-luarocks \
+  mailutils \
+  make \
+  mc \
+  mutt \
+  ncdu \
+  neovim \
+  nnn \
+  powerline \
+  ripgrep \
+  stow \
+  tmux \
+  trash-cli \
+  tree \
+  vim \
+  wl-clipboard \
+  wget \
+  xclip \
+  zsh
+
+pip install trash-cli
+
 
 read -n1 -p $'Does this system need ssh access?\n' REPLY
-	if [[ $REPLY == [Yy] ]]; then 
-		sudo zypper install openssh-server fail2ban
-	fi
+if [[ $REPLY == [Yy] ]]; then
+  sudo zypper install openssh-server fail2ban
+  trash ~/.zshrc_prelocal
+fi
 read -n1 -p $'\nDoes this system have a GUI?\n' REPLY
-	if [[ $REPLY == [Yy] ]]; then 
-		sudo zypper install emacs 
-	else 
-		sudo zypper install emacs-nox
-	fi
-sudo zypper install "https://github.com/muesli/duf/releases/download/v0.8.1/duf_0.8.1_linux_amd64.rpm"
+if [[ $REPLY == [Yy] ]]; then
+  flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+fi
+#setup git-delta
+git config --global core.pager delta
+git config --global interactive.diffFilter 'delta --color-only'
+git config --global delta.navigate true
+git config --global merge.conflictStyle zdiff3
+  echo "Install lazygit"
+  LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+  curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+  tar xf lazygit.tar.gz lazygit
+  sudo install lazygit -D -t /usr/local/bin/
+  trash lazygit lazygit.tar.gz
