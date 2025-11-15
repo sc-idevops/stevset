40db360dfa1771a9d889b820e99606f6d681b57a
sc-idevops <steveokard@gmail.com>
2025-11-15 02:13:55 -0500

    add sleep

--- scripts/server_bin/update_twitter.sh
+++ scripts/server_bin/update_twitter.sh
@@ -2,12 +2,12 @@
 rm ~/twit_error.txt
 cd $HOME/gallery-dl/twitter
 echo ">>>> Begin Downloads"
-gallery-dl --abort 3 -o retweets=true "https://twitter.com/stefenauris"
-gallery-dl --abort 3 -o retweets=true "https://twitter.com/DaybreakHero"
+gallery-dl --abort 3 --sleep 2-3 -o retweets=true "https://twitter.com/stefenauris"
+gallery-dl --abort 3 --sleep 2-3 -o retweets=true "https://twitter.com/DaybreakHero"
 for i in */; do
   echo ">> $i"
   date
-  gallery-dl --sleep 3-4 --abort 3 "https://twitter.com/$i" || echo "$i" >> ~/twit_error.txt
+  gallery-dl --sleep 3-4 --abort 3 "https://twitter.com/$i" || echo "$i" >>~/twit_error.txt
   echo "    << waiting"
   sleep 25
 done
