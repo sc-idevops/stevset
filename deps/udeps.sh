296f43a4080b71458cd3db3f2e4f06e2bf4121b9
steveokard <steveokard@gmail.com>
2026-04-25 22:00:21 -0400

    updates for resolute

--- deps/udeps.sh
+++ deps/udeps.sh
@@ -65,26 +65,34 @@ if [[ $REPLY == [Yy] ]]; then
 fi
 
 #This section installs software outside apt
-read -n1 -p $'\nDownload external packages?\n' REPLY
-if [[ $REPLY == [Yy] ]]; then
-  dpkg_url https://github.com/dandavison/delta/releases/download/0.18.2/git-delta_0.18.2_amd64.deb
+if [[ $(lsb_release -cs) == "resolute" ]]; then
+  sudo apt install git-delta du-dust glow btm bat fd-find
   git config --global core.pager delta
   git config --global interactive.diffFilter 'delta --color-only'
   git config --global delta.navigate true
   git config --global merge.conflictStyle zdiff3
-  dpkg_url https://github.com/bootandy/dust/releases/download/v1.2.4/du-dust_1.2.4-1_amd64.deb
-  dpkg_url https://github.com/charmbracelet/glow/releases/download/v2.1.1/glow_2.1.1_amd64.deb
-  dpkg_url https://github.com/ClementTsang/bottom/releases/download/0.12.3/bottom_0.12.3-1_amd64.deb
-  dpkg_url https://github.com/sharkdp/bat/releases/download/v0.26.1/bat_0.26.1_amd64.deb
-  dpkg_url https://github.com/sharkdp/fd/releases/download/v10.3.0/fd_10.3.0_amd64.deb
-  dpkg_url http://mirrors.kernel.org/ubuntu/pool/universe/f/fzf/fzf_0.60.3-1_amd64.deb
+else
+  read -n1 -p $'\nDownload external packages?\n' REPLY
+  if [[ $REPLY == [Yy] ]]; then
+    dpkg_url https://github.com/dandavison/delta/releases/download/0.18.2/git-delta_0.18.2_amd64.deb
+    git config --global core.pager delta
+    git config --global interactive.diffFilter 'delta --color-only'
+    git config --global delta.navigate true
+    git config --global merge.conflictStyle zdiff3
+    dpkg_url https://github.com/bootandy/dust/releases/download/v1.2.4/du-dust_1.2.4-1_amd64.deb
+    dpkg_url https://github.com/charmbracelet/glow/releases/download/v2.1.1/glow_2.1.1_amd64.deb
+    dpkg_url https://github.com/ClementTsang/bottom/releases/download/0.12.3/bottom_0.12.3-1_amd64.deb
+    dpkg_url https://github.com/sharkdp/bat/releases/download/v0.26.1/bat_0.26.1_amd64.deb
+    dpkg_url https://github.com/sharkdp/fd/releases/download/v10.3.0/fd_10.3.0_amd64.deb
+    dpkg_url http://mirrors.kernel.org/ubuntu/pool/universe/f/fzf/fzf_0.60.3-1_amd64.deb
 
-  echo "Install lazygit"
-  LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
-  curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-  tar xf lazygit.tar.gz lazygit
-  sudo install lazygit -D -t /usr/local/bin/
-  trash lazygit lazygit.tar.gz
+    echo "Install lazygit"
+    LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+    curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+    tar xf lazygit.tar.gz lazygit
+    sudo install lazygit -D -t /usr/local/bin/
+    trash lazygit lazygit.tar.gz
+  fi
 fi
 
 #script to link to stevserver over LAN. Note: the username in the filename and file have to match yours.
