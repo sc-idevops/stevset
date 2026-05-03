eee7345c14f52edb8efdcc3c2cc7bafe452375b1
sc-idevops <steveokard@gmail.com>
2026-05-03 01:37:29 -0400

    fixed issues

--- scripts/server_bin/update_ytmusic.sh
+++ scripts/server_bin/update_ytmusic.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 cd "/home/stev/Pictures/Music/Liked Music/"
-yt-dlp --embed-metadata --parse-metadata "playlist_index:%(track_number)s" -x --audio-format mp3 --audio-quality 5 --embed-thumbnail --cookies-from-browser firefox --download-archive index.txt --break-on-existing 'https://music.youtube.com/playlist?list=LM'
+yt-dlp --embed-metadata --parse-metadata "playlist_index:%(track_number)s" -x --audio-format mp3 --audio-quality 5 \
+  --embed-thumbnail --cookies ~/.gallery-dl/cookies-youtube-com.txt \
+  --remote-components ejs:github --download-archive index.txt \
+  --break-on-existing 'https://music.youtube.com/playlist?list=LM'
