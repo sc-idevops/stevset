dbd1da1e67518a73ec198c3bed69d5fcebcf506b
sc-idevops <steveokard@gmail.com>
2026-08-28 14:35:41 -0400

    added size comparison

--- scripts/server_bin/db_maint.sh
+++ scripts/server_bin/db_maint.sh
@@ -2,5 +2,7 @@
 #vaccum the database
 echo "--- DATABASE MAINTENANCE ---"
 cd $HOME/gallery-dl
+ls -alh *.sqlite3
 sqlite3 archive.sqlite3 'VACUUM;'
+ls -alh *.sqlite3
 echo "--- MAINTENANCE COMPLETE ---"
